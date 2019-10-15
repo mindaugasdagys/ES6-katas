2202400f9503b32c32409321cbb9d8c45165d625
mindaugasdagys <admin@mindaugasdagys.com>
2019-10-15 15:26:16 +0300

    add README file

---
+++ arrow-functions.js
@@ -0,0 +1,22 @@
+
+function sum(a, b) {
+    return a + b
+}
+
+const sum2 = (a, b) => {
+    return a + b
+}
+
+function isPositive(number) {
+    return number >= 0
+}
+
+const isPositive2 = number => number >= 0
+
+function randomNumber() {
+    return Math.random
+}
+
+const random2 = () => Math.random
+
+document.addEventListener('click', () => console.log('Click')) 
